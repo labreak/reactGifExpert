4e78c3f0303be696b39d86e3b00a0d96dda56f32
Nicolas Padilla <padillanico@gmail.com>
2022-07-20 00:05:38 -0300

    Se termino Test

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,20 @@
+const { render,screen } = require("@testing-library/react")
+const { GifExpertApp } = require("../src/GifExpertApp")
+
+describe('first', () => { 
+
+test('should first', () => { 
+
+render(<GifExpertApp/>);
+screen.debug();
+
+
+
+
+ })
+
+
+
+
+
+ })
